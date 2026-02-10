4b95cda7c761fd66241834b59ef83585e91ca2e8
Ralph Ramos <ralphmtk@gmail.com>
2026-02-10 19:54:12 -0300

    Git Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
+// import { defineConfig } from "vite"
+// import react from "@vitejs/plugin-react"
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+// })
+
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     hmr: {
       overlay: false,
     },
+  },
+
+  // 🔥 Importante para GitHub Pages
+  base: "/advinhe/",
+
+  build: {
+    outDir: "docs",
   },
 })
